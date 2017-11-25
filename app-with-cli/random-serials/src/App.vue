d09d166a81cb42ddedd563d4c4deafd4e57a2f98
lukovskij <kravchuk1995@ukr.net>
2017-11-25 16:06:46 +0300

    add vue cli app

---
+++ app-with-cli/random-serials/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+ 
+<div>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col">
+        <div class="navbar  navbar-dark bg-dark d-flex justify-content-end">
+          <form action="" class="from-inline d-flex">
+            <div>
+              <button class="btn btn-outline-succes mr-3" type="button" @click="switchSign('sign-in')">Log in</button>
+              <button class="btn btn-outline-success" type="button" @click="switchSign('sign-up')"> Register </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <sigin-in v-if="sign == 'sign-in'"></sigin-in>
+        <sigin-up v-else></sigin-up>
+      </div>
+    </div>
+  </div>
+</div>
+
+</template>
+
+<script>
+import SiginIn from './components/SiginIn';
+import SiginUp from './components/SiginUp';
+export default {
+  name: 'app',
+  data () {
+    return {
+      sign : 'sign-in'
+    }
+  },
+  components : { // показываем какие компоненты будем использовать внутри шаблона
+     SiginIn,
+     SiginUp
+  },
+  methods : {
+    switchSign(switcher) {
+       this.sign = switcher
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ app-with-cli/random-serials/src/components/SiginIn.vue
@@ -0,0 +1,5 @@
+<template>
+  <h2>
+      SiginIn
+  </h2>
+</template>
